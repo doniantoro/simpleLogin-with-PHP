97d30831cda0ed0c3ab8d1c8b1f825e60c3f3be6
Doni Antoro <53077980+doniantoro@users.noreply.github.com>
2019-11-08 17:18:16 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,62 +3,57 @@
     <title>Login-Pilih Kampus</title>
   </head>
   <body>
-		<div class="kotak">
-			
+		<div class="kotak">			
 			<div class="gambar">
-				<img src="IMG/img_avatar.png" alt="ava" class="ava">
+				<img src="img/img_avatar.png" alt="ava" class="ava">
 			</div>
-
-			<center><h3>SILAKHAN LOGIN</h3>	</center><br>		
-			  		
-					<form method="post">
-					<label><b> EMAIL </b></label>
-					<input type="text" name="email_login" id="email_input"  class="login" placeholder="Masukan email"> <br>
-					<label><b> Password	</b></label>
-					<input type="password" name="pass_login" id="password_input" class="login" placeholder="Masukan Password"> 
-					<input type="submit">
-					</form>
-					<hr>
-					
-                    
-			</div>
+			
+			<center><h3>LOGIN PAGE</h3></center><br>		
+			  <form method="post">
+				<label><b> EMAIL </b></label>
+				<input type="text" name="email_login" id="email_input"  class="login" placeholder="Input email"> <br>
+				<label><b> Password	</b></label>
+				<input type="password" name="pass_login" id="password_input" class="login" placeholder="Input password"> 
+				<input type="submit">
+			</form><hr>
+		</div>
  
-
 			
 			
 	
 		<?php
 		if ($_SERVER["REQUEST_METHOD"] == "POST"){	
 			include 'php/koneksi.php';
-	        //Escape function untuk mencegah injection
+	        //Escape function to forbide sql injection
 			$email_login = mysqli_real_escape_string ($conn,$_POST['email_login']);
 			$pass_login = mysqli_real_escape_string ($conn,$_POST['pass_login']);
-			$cek      = " select * from login where email_login='$email_login'";
+			$sql      = " select * from login where email_login='$email_login'";
 			
-			$result1=$conn->query($cek);
-			$result2=$conn->query($cek);
+			$result1=$conn->query($sql);
+			$result2=$conn->query($sql);
 			
 			$user = mysqli_fetch_assoc($result2);
 			
 			//Enkripsi Password
 			$hash= password_hash('$pass_login',PASSWORD_DEFAULT);
-            if(password_verify('$pass_login',$user['pass_login'])){
-			$data = mysqli_fetch_array($result1);
-             }
+            		if(password_verify('$pass_login',$user['pass_login'])){
+				$data = mysqli_fetch_array($result1);
+             		}
              
-			if($data>0){
-			
-			if ($data['kelas_login'] == 'developer') {
-					session_start();
-					$_SESSION['id_login'] = $data['id_login'];
-					$id=$_SESSION['id_login'];
-				echo "<script>alert('Selamat Datang, Developer.');document.location.href='../../'</script>";
+			if($data>0)
+			{
+				//get session
+				session_start();
+				$_SESSION['id_login'] = $data['id_login'];
+				$id=$_SESSION['id_login'];
+				echo "<script>alert('Wellcome there.');document.location.href='../../'</script>";
 			}
 			
-				}else{
-					echo "<script>alert('Email/password anda salah');document.location.href='login.php'</script>";
-		}
-		}
+				else
+				{
+					echo "<script>alert('Email/password is wrong');document.location.href='login.php'</script>";
+				}
+			}
 		?>
 
 
